b9b8a83ad0bd9d6c6aa485107475b31c4f080744
ligoudan5243 <150013646+ligoudan5243@users.noreply.github.com>
2026-03-02 22:11:14 +0800

    Update adminIndex.js

--- templates/admin/adminIndex.js
+++ templates/admin/adminIndex.js
@@ -2,15 +2,15 @@
 import { adminHeaderHTML } from './adminHeader.js';
 import { addProjectHTML } from './addProject.js';
 import { autoMonitorHTML } from './autoMonitor.js';
-import { hostnameHTML } from './hostname.js';
 import { s3ConfigHTML } from './s3Config.js';
+import { hostnameHTML } from './hostname.js';
 
 export const adminHTML = `
 <div id="adminPanel" class="admin-panel">
     ${adminHeaderHTML}
     ${addProjectHTML}
     ${autoMonitorHTML}
-    ${hostnameHTML}
     ${s3ConfigHTML}
+    ${hostnameHTML}
 </div>
 `;
